f55a7b88ea9525d4e7a1d28077b5bfd45c139e31
Bladix99YT <maikon15489566@hotmail.com>
2023-03-28 14:22:08 -0400

    Aula c# poo2 com o framework 7.0

---
+++ AtacadoConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace AtacadoConsole;
+using Atacado.BD.EF.Database;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        AtacadoContext contexto = new AtacadoContext();
+        foreach (Categorium item in contexto.Categoria.ToList())
+        {
+            Console.WriteLine($"{item.Codigo} - {item.Descricao}");
+        }
+
+        Console.ReadLine();
+    }
+}
